611aeb6903c982e412b79db70cb40031e4d074ef
Andrea Giardina <Andregiardi151@gmail.com>
2026-03-25 22:20:57 +0100

    update

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./landing.module.css";
 
 export default function LandingPage() {
   return (
